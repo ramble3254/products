26676168eff7ff1c7f74fd74bbb432824b4ab20e
SmallK Lee <ramble3254@gmail.com>
2019-05-22 15:35:12 +0800

    code arrange

--- products.py
+++ products.py
@@ -10,6 +10,7 @@ with open('product.csv', 'r', encoding='utf-8') as f:
 print(products)
 #strip去除換行,split切割 #切割完後直接存成name跟price
 
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -21,10 +22,14 @@ while True:
 #p.append(name)
 #p.append(price)
 #products.append(p)
-print(products[0][1]) #意思為進去products[0]取[1]資料
+#print(products[0][1]) #意思為進去products[0]取[1]資料
+
+# 印出購買紀錄
 for product in products:
 	print(product[0], '的價格是', product[1])
-with open('product.txt', 'w', encoding='utf-8') as f:
+
+#寫入檔案
+with open('product.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
